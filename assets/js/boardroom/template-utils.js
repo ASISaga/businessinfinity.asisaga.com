b23dbbf8bb4aada57f35c74ed67f5d79e43705f4
Shabeer Dewan <chebyr@gmail.com>
2025-06-28 23:15:06 +0530

    Update template syntax from Mustache to custom bracket notation in boardroom templates

--- assets/js/boardroom/template-utils.js
+++ assets/js/boardroom/template-utils.js
@@ -11,18 +11,18 @@ class TemplateUtils {
   }
 
   static renderTemplate(template, data) {
-    return template.replace(/{{(\w+)}}/g, (match, key) => {
+    return template.replace(/\[\[(\w+)\]\]/g, (match, key) => {
       return key in data ? data[key] : '';
     });
   }
 
   static renderLogicBlocks(template, data) {
     // #if
-    template = template.replace(/{{#if (\w+)}}([\s\S]*?){{\/if}}/g, (m, key, content) => {
+    template = template.replace(/\[\[#if (\w+)\]\]([\s\S]*?)\[\[\/if\]\]/g, (m, key, content) => {
       return data[key] ? content : '';
     });
     // #unless
-    template = template.replace(/{{#unless (\w+)}}([\s\S]*?){{\/unless}}/g, (m, key, content) => {
+    template = template.replace(/\[\[#unless (\w+)\]\]([\s\S]*?)\[\[\/unless\]\]/g, (m, key, content) => {
       return !data[key] ? content : '';
     });
     return template;
