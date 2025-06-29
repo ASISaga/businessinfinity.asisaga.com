64bea40e0716398ed7b263f3beb682b19f4da98f
Shabeer Dewan <chebyr@gmail.com>
2025-06-29 06:08:02 +0530

    corrected script path

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Central script loader for Boardroom | Business Infinity
 import 'https://kit.fontawesome.com/a076d05399.js';
 import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js';
-import './boardroom.js';
+import './boardroom/boardroom.js';
 
 document.addEventListener('DOMContentLoaded', function () {
   const app = new Boardroom();
