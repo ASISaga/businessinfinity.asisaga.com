c230e0ac4fb1c0dde2331f6fad67e124404953f9
Shabeer Dewan <chebyr@gmail.com>
2025-06-29 15:32:42 +0530

    Common scripting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 // Central script loader for Boardroom | Business Infinity
-import 'https://kit.fontawesome.com/a076d05399.js';
-import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js';
+import './common.js';
 import './boardroom/boardroom.js';
 
 document.addEventListener('DOMContentLoaded', function () {
