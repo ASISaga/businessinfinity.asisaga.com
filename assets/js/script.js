72af599917c0c3345667bbe8106029cc26e33e8e
Shabeer Dewan <chebyr@gmail.com>
2025-07-02 22:33:01 +0530

    boardroom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // Central script loader for Boardroom | Business Infinity
 import './common.js';
-import './boardroom/boardroom.js';
+import Boardroom from './boardroom/boardroom.js';
 
+// Initialize the Boardroom application when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function () {
   const app = new Boardroom();
   app.init();
